806582c73fccd723ce69062de081098f23b365a7
Portfedh <59422723+portfedh@users.noreply.github.com>
2025-05-30 22:40:36 -0600

    Chapter 2. Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
+// Any files added to this directory become a screen inside our native app and a page on the web.
+
+// Defines shared UI elements such as headers and tab bars
+// so they are consistent between different routes
+
+// Index file names, such as index.tsx,
+// match their parent directory and do not add a path segment
+
+// Files  can be .js, .jsx, .ts, or .tsx
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
